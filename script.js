6619a1e2b60a414058b941f124ea01b751180e37
madosy <dasomee@outlook.com>
2022-08-25 12:08:36 -0400

    Create basic math operators
    add(), subtract(), multiply(), divide() takes 2 arguments, parse
    float, run operation in order of arguments then returns the result.

---
+++ script.js
@@ -0,0 +1,16 @@
+//basic math operators
+const add = function(a,b) {
+    return parseFloat(a) + parseFloat(b)
+}
+
+const subtract = function(a,b) {
+    return parseFloat(a) - parseFloat(b)
+}
+
+const multiply = function(a,b) {
+    return parseFloat(a) * parseFloat(b)
+}
+
+const divide = function(a,b) {
+    return parseFloat(a) / parseFloat(b)
+}
