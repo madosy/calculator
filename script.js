51e3834b1557cadc08cb7e11a6709d3865a9c2c2
Dasom Heo <dasomee@outlook.com>
2022-10-09 10:49:29 -0400

    Store 1st value after entering 2nd then operator Store 2nd value after entering operator again
    Changed way the values are stored so that user can switch operators
    before calculating the value.

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 //basic math operators
-const add = function(a,b) {
-    return parseFloat(a) + parseFloat(b)
-}
-
-const subtract = function(a,b) {
-    return parseFloat(a) - parseFloat(b)
-}
-
-const multiply = function(a,b) {
-    return parseFloat(a) * parseFloat(b)
-}
-
-const divide = function(a,b) {
-    return parseFloat(a) / parseFloat(b)
-}
+const add = function(a,b) {return parseFloat(a) + parseFloat(b)}
+const subtract = function(a,b) {return parseFloat(a) - parseFloat(b)}
+const multiply = function(a,b) {return parseFloat(a) * parseFloat(b)}
+const divide = function(a,b) {return parseFloat(a) / parseFloat(b)}
 
 var inputElement = document.querySelector('.input-container');
 var decimalPresent = false;
@@ -38,18 +27,7 @@ function appendInput(a) {
     inputElement.textContent += a
 }
 
-//grab all num buttons
-var numButtons = document.querySelectorAll('div.num.button')
-numButtons.forEach((button) => {
-    button.addEventListener('click', ()=>appendInput(button.textContent))
-    button.addEventListener('click', ()=>{
-        if (clearDisplay) {
-            updateDisplay(button.textContent)
-            clearDisplay = false;
-        }
-    })
-})
-
+//mod buttons
 var modButtons = document.querySelectorAll('div.mod.button')
 var modButton_clear = modButtons[0].addEventListener('click', ()=>{
     updateDisplay('0')
@@ -59,7 +37,69 @@ var modButton_clear = modButtons[0].addEventListener('click', ()=>{
 var modButton_invert = modButtons[1].addEventListener('click', ()=>updateDisplay(getInput()*(-1)))
 var modButton_percent = modButtons[2].addEventListener('click', ()=>updateDisplay(getInput()/100))
 
+//grab all num buttons
+var numButtons = document.querySelectorAll('div.num.button')
+numButtons.forEach((button) => {
+    //if there is an operator, and there is no first value, store the value
+    button.addEventListener('click', () => {
+        if (operator != '' && valArr.length < 1) storeValue(getInput())
+    })
+    //clear display if operator was pressed
+    button.addEventListener('click', ()=> {
+        if (operatorPressed) {
+            updateDisplay('')
+            operatorPressed = false;
+        }
+    })
+    button.addEventListener('click', ()=>appendInput(button.textContent))
+})
+
 var opButtons = document.querySelectorAll('div.op > div.button')
+let operator = ''
+let operatorPressed = false;
+for (let index = 0; index < (opButtons.length -1); index++) {
+    opButtons[index].addEventListener('click', () => {
+        // if (equalityPressed == false) storeValue(getInput())
+        // if (valArr.length > 1) {
+        //     result = operate(operator)
+        //     savePop(result)
+        //     updateDisplay(result)
+        // }
+        //if there is a first and 2nd value, operate then update the display
+        // if (valArr.length > 1) {
+        //     result = operate(operator)
+        //     updateDisplay(result)
+        //     valArr.pop();
+        // }
+
+        // if there is a first value and an operator, store the value
+        if (valArr.length == 1 && operator != '') {
+            storeValue(getInput())
+        }
+    // opButtons[index].addEventListener('click', ()=> {
+    //     //if there is a first value and an operator, store the value
+    //     if (valArr.length == 1 && operator != '') {
+    //         storeValue(getInput())
+    //     }
+    // })
+
+        operator = opButtons[index].textContent
+        operatorPressed = true;
+    })
+}
+
+function operate(operator){
+    a = valArr[0]
+    b = valArr[1]
+    let result = 0
+
+    if (operator == '+') result = add(a,b)
+    else if (operator == '−') result = subtract(a,b)
+    else if (operator == '×') result = multiply(a,b)
+    else if (operator == '÷') result = divide(a,b)
+
+    return result;
+}
 
 let valArr = [];
 function storeValue(a) {
@@ -67,64 +107,26 @@ function storeValue(a) {
     else valArr[1] = a
 }
 
-let operator = ''
-clearDisplay = false
-// opButtons.forEach((button) => {
-//     button.addEventListener('click', () => {
-//         console.log(operator)
-//         storeValue(getInput())
-//         if (valArr.length > 1) operate(operator)
-//         if (button.textContent != '=') operator = button.textContent
-        
-//         condition = true
-//     })
+
+// clearDisplay = false
+
+
+
+// equalityPressed = false;
+// opButtons[4].addEventListener('click', () => {
+//     equalityPressed = true;
+//     storeValue(getInput())
+//     if (valArr.length > 1) {
+//         result = operate(operator)
+//         updateDisplay(result)
+//     }
+//     // valArr=[]
 // })
 
-for (let index = 0; index < (opButtons.length -1); index++) {
-    opButtons[index].addEventListener('click', () => {
-        if (equalityPressed == false) storeValue(getInput())
-        if (valArr.length > 1) {
-            result = operate(operator)
-            savePop(result)
-            updateDisplay(result)
-        }
-        operator = opButtons[index].textContent
-        clearDisplay = true;
-        equalityPressed = false;
-    })
-}
-
-equalityPressed = false;
-opButtons[4].addEventListener('click', () => {
-    equalityPressed = true;
-    storeValue(getInput())
-    if (valArr.length > 1) {
-        result = operate(operator)
-        updateDisplay(result)
-    }
-    // valArr=[]
-})
-
-function operate(operator){
-    a = valArr[0]
-    b = valArr[1]
-
-    if (operator == '+'){
-        var result = add(a,b)
-    } else if (operator == '−') {
-        var result = subtract(a,b)
-    } else if (operator == '×') {
-        var result = multiply(a,b)
-    } else if (operator == '÷') {
-        var result = divide(a,b)
-    }
-    return result;
-}
-
-function savePop(result) {
-    valArr[0] = result;
-    valArr.pop();
-}
+// function savePop(result) {
+//     valArr[0] = result;
+//     valArr.pop();
+// }
 
 
 /* if I click an operator and I don't have a stored value, store to 1st spot.*/
